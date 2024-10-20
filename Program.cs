f1bacf0f8943f2802d58899ce5be829a246ca207
Katelli <aska082024@gmail.com>
2024-10-20 15:31:03 +0200

    Completed SumTwoNumbersOfGivenType

--- Program.cs
+++ Program.cs
@@ -11,14 +11,21 @@ class Program
         // Japanese greeting
         greetings.YoiTsuitachi();
 
+        // Create instance
         Fundament fundament = new Fundament();
 
+        // Odd or even
         fundament.CheckOddOrEven(5);
 
+        // Interesting fact
         string fact = "Um, turtles can breathe through their butts? -Olaf, the snowman";
         Console.WriteLine(fundament.GetAnInterestingFact(fact));
 
+        // Sum two numbers
         fundament.SumTwoNumbers();
+
+        // Sum two numbers of a given type
+        Console.WriteLine($"The sum of 5.5 + 6.8 is {fundament.SumTwoNumbersOfGivenType(5.5, 6.8)}");
 
     }
 }
